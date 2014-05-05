0b320dca34d945c94a1e4a2715874483bfcef5ea
Wyatt <wyatt.dorn@gmail.com>
2014-05-05 15:36:49 -0700

    vim added

---
+++ two_shaders_glut/vim/src/auto/pathdef.c
@@ -0,0 +1,10 @@
+/* pathdef.c */
+/* This file is automatically created by Makefile
+ * DO NOT EDIT!  Change Makefile only. */
+#include "vim.h"
+char_u *default_vim_dir = (char_u *)"/usr/local/share/vim";
+char_u *default_vimruntime_dir = (char_u *)"";
+char_u *all_cflags = (char_u *)"gcc -c -I. -Iproto -DHAVE_CONFIG_H -DFEAT_GUI_GTK  -I/usr/local/include/gtk-2.0 -I/usr/local/include/gio-unix-2.0/ -I/usr/local/include/atk-1.0 -I/usr/local/include/cairo -I/usr/local/include/pixman-1 -D_THREAD_SAFE -I/usr/local/include/gdk-pixbuf-2.0 -I/usr/local/include/libpng15 -I/usr/local/include/pango-1.0 -pthread -I/usr/local/include/glib-2.0 -I/usr/local/include  -I/usr/local/include/freetype2 -I/usr/local/include/harfbuzz   -I/usr/local/include  -O2 -fno-strength-reduce -Wall -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=1      ";
+char_u *all_lflags = (char_u *)"gcc -L/usr/local/lib    -L/usr/local/lib -Wl,--as-needed -o vim   -lgtk-x11-2.0 -lgdk-x11-2.0 -lpangocairo-1.0 -lXext -lXrender -lXinerama -lXi -lXrandr -lXcursor -lXcomposite -lXdamage -lXfixes -lX11 -latk-1.0 -lcairo -pthread -lgdk_pixbuf-2.0 -lgio-2.0 -lpangoft2-1.0 -lpango-1.0 -lgobject-2.0 -lglib-2.0 -lintl -lfontconfig -L/usr/local/lib -lfreetype   -lSM -lICE -lXpm -lXt -lX11   -lm -ltinfo -lelf  -liconv -lintl          ";
+char_u *compiled_user = (char_u *)"me";
+char_u *compiled_sys = (char_u *)"srl-freebsd92-amd64";
